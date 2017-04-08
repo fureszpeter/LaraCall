7c4c994678a35f296cb281764bc4283fac7a7fe1
Peter Furesz <fureszpeter@gmail.com>
2017-04-08 21:43:13 +0200

    Extend notification info

--- app/Events/PaymentHandlers/SendPaymentReversedNotification.php
+++ app/Events/PaymentHandlers/SendPaymentReversedNotification.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace LaraCall\Events\PaymentHandlers;
 
+use A2bApiClient\Client;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Message;
 use Illuminate\Queue\InteractsWithQueue;
 use LaraCall\Domain\Repositories\PayPalIpnRepository;
 use LaraCall\Events\PaymentReversedEvent;
+use LaraCall\Infrastructure\Services\Ebay\EbayApiService;
 use Snowfire\Beautymail\Beautymail;
 
 class SendPaymentReversedNotification implements ShouldQueue
@@ -23,30 +26,56 @@ class SendPaymentReversedNotification implements ShouldQueue
     private $beautymail;
 
     /**
+     * @var Client
+     */
+    private $a2bClient;
+
+    /**
+     * @var EbayApiService
+     */
+    private $ebayApiService;
+
+    /**
      * @param PayPalIpnRepository $payPalIpnRepository
      * @param Beautymail          $beautymail
+     * @param Client              $a2bClient
+     * @param EbayApiService      $ebayApiService
      */
     public function __construct(
         PayPalIpnRepository $payPalIpnRepository,
-        Beautymail $beautymail
+        Beautymail $beautymail,
+        Client $a2bClient,
+        EbayApiService $ebayApiService
     ) {
         $this->payPalIpnRepository = $payPalIpnRepository;
         $this->beautymail          = $beautymail;
+        $this->a2bClient           = $a2bClient;
+        $this->ebayApiService      = $ebayApiService;
     }
 
     public function handle(PaymentReversedEvent $event)
     {
         $ipn = $this->payPalIpnRepository->get($event->getIpnId());
-        if (!$ipn->isEbay())
-        {
+        if ( ! $ipn->isEbay()) {
             return;
         }
 
         $subscription = $ipn->getParentIpn()->getSubscription();
 
-        $data         = [
-            'subject'      => 'Payment reversed! What should I do now?',
-            'name'         => $subscription->getFirstName() . $subscription->getLastName(),
+        $getUserResponse          = $this->ebayApiService->getUser($ipn->getEbayUsername());
+        $userEbayRegistrationDate = $getUserResponse->User->RegistrationDate;
+
+
+        $apiSubscription = $this->a2bClient->getSubscription()->getByPin($subscription->getDefaultPin()->getPin());
+        $apiSubscription->credit;
+        $data = [
+            'subject'  => 'Payment reversed! What should I do now?',
+            'credit'   => $apiSubscription->credit,
+            'lastUsed' => $apiSubscription->lastuse,
+            'pinCount' => $subscription->getPins()->count(),
+            'ebayRegistrationDate' => $userEbayRegistrationDate->format(DATE_ATOM),
+
+            'name'         => $subscription->getFirstName().$subscription->getLastName(),
             'email'        => $subscription->getUser()->getEmail(),
             'pins'         => $subscription->getPins()->toArray(),
             'country'      => $subscription->getCountry()->getCountryName(),
