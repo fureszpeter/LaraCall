13310bd226f5556378652229ab80143b9d18a444
Peter Furesz <fureszpeter@gmail.com>
2017-06-25 22:03:52 +0200

    Add test failed job command

---
+++ app/Jobs/ForceFailJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LaraCall\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ForceFailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        throw new Exception('Test job fail exception!');
+    }
+}
